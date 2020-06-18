4f6b835a6f17bb229606d39d4a3fce70ee8a3f3b
Gaylord Poillon <gaylord.p@gmail.com>
2020-06-18 14:26:28 +0200

    Maj

--- Resources/public/color.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery'
-
-$(document).ready(function () {
-    $('.form-color-select').click(function () {
-        $(this).find('input').prop('checked', true)
-    })
-})
---
+++ Resources/public/js/color.js
@@ -0,0 +1,7 @@
+document.onclick = (e) => {
+    let color = e.target.closest('.form-color-select')
+
+    if (null !== color) {
+        color.querySelector('input').checked = true
+    }
+}
